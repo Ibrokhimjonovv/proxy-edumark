986b520e26ba33afa1e7f027bd1701a5c74a2b91
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2025-04-09 21:45:00 +0500

    .

---
+++ apiProxy.jsx
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+app.use(cors());
+
+// API endpointlarini proksi orqali yuborish
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'https://api.edumark.uz',
+    changeOrigin: true,
+    pathRewrite: { '^/api': '/api' },
+  })
+);
+
+// Order uchun alohida proxy (agar boshqa port yoki server bo'lsa)
+app.use(
+  '/order/create',
+  createProxyMiddleware({
+    target: 'https://api.edumark.uz/api/order/create',
+    changeOrigin: true,
+    pathRewrite: { '^/order': '' }, // faqat /order/create kabi requestlar bo‘lsa
+  })
+);
+
+// Media fayllar uchun
+app.use(
+    '/api/media',
+    createProxyMiddleware({
+      target: 'http://api.edumark.uz',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/media': '/media',
+      },
+    })
+  );
+  
+
+// Serverni ishga tushirish
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Proxy server http://localhost:${PORT} da ishga tushdi!`);
+});
